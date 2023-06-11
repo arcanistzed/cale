0bb92f3363d834c87f897736176b29f933606bd2
Daniel Thorp <arcanistzed@gmail.com>
2023-06-11 02:38:49 -0400

    fix: configuration for deployment to GitHub Pages
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import astroI18next from "astro-i18next";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), astroI18next()],
+	base: "/cale",
+	site: "https://arcanist.me/cale",
 });

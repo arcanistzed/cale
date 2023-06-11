dd78cf57c47733341861ba08246b4080b5d1d7c8
Daniel Thorp <arcanistzed@gmail.com>
2023-06-11 02:46:07 -0400

    ci: change to use Cloudflare Pages
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,4 @@ import astroI18next from "astro-i18next";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), astroI18next()],
-	base: "/cale",
-	site: "https://arcanist.me/cale",
 });
